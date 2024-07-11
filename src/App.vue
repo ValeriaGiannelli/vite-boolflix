64b26fe6875ce3c32db0970a2dce873f77057327
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-11 11:31:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  
+}
+</script>
+
+<template>
+
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss">
+
+</style>
