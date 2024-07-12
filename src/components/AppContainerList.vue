8f195d338f1f258c79b6414edf15fe10f4e6c3da
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-12 10:30:24 +0200

    feat: add a starting page an hidden when the user search a title

--- src/components/AppContainerList.vue
+++ src/components/AppContainerList.vue
@@ -20,27 +20,53 @@ export default {
 </script>
 
 <template>
-    <h2>Film</h2>
-    <div class="container">
-        
-        <!-- card dei film ripresa dal componente CardMovie -->
-        <CardFilm v-for="film in store.filmList" :key="film.id" :infoFilm="film"/>
-
+    <!-- schermata iniziale -->
+    <div class="start_page" :class="{'hidden' : store.searchTitle}">
+        <h1>Ricerca il tuo film o serie tv</h1>
     </div>
 
-    <h2>Serie</h2>
-    <div class="container">
-        
-        <!-- card dei film ripresa dal componente CardSerie -->
-        <CardSerie v-for="serie in store.serieList" :key="serie.id" :infoSerie="serie"/>
+    <!-- divisione della ricerca -->
+    <div :class="{'hidden' : !store.searchTitle}">
+        <h2>Film</h2>
+        <div class="container">
+            
+            <!-- card dei film ripresa dal componente CardMovie -->
+            <CardFilm v-for="film in store.filmList" :key="film.id" :infoFilm="film"/>
 
+        </div>
+
+        <h2>Serie</h2>
+        <div class="container">
+            
+            <!-- card dei film ripresa dal componente CardSerie -->
+            <CardSerie v-for="serie in store.serieList" :key="serie.id" :infoSerie="serie"/>
+
+        </div>
     </div>
+   
 
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+
+.start_page{
+    background-color: gray;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h1{
+        font-size: 50px;
+    }
+}
+
 .container{
     flex-wrap: wrap;
     gap: 10px;
+}
+
+.hidden{
+    display:none;
 }
 </style>
